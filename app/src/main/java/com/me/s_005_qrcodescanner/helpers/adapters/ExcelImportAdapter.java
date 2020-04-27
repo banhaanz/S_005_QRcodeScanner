32379cdc530948c612bde15ff8e906b1363a4129
banhaanz <49148187+banhaanz@users.noreply.github.com>
2020-04-27 16:41:49 +0700

    Big update 1
    - add check-scanned feature
    - fix some bugs

--- app/src/main/java/com/me/s_005_qrcodescanner/helpers/adapters/ExcelImportAdapter.java
+++ app/src/main/java/com/me/s_005_qrcodescanner/helpers/adapters/ExcelImportAdapter.java
@@ -1,5 +1,6 @@
 package com.me.s_005_qrcodescanner.helpers.adapters;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,16 +19,20 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ExcelImportAdapter extends RecyclerView.Adapter<ExcelImportAdapter.ViewHolder> {
-    private List<String> titles,descriptions,images,nums,statuss;
+    private List<String> titles,descriptions,images,nums,statuss,scanneds,timestamps;
     private LayoutInflater inflater;
 
-    public ExcelImportAdapter(Context context, List<String> num, List<String> descriptions, List<String> images, List<String> titles, List<String> status){
+    public ExcelImportAdapter(Context context, List<String> num, List<String> descriptions,
+                              List<String> images, List<String> titles, List<String> status,
+                              List<String> scanned, List<String> timestamp){
         Log.d("data", "titles -> "+titles);
         this.titles = titles;
         this.descriptions = descriptions;
         this.images = images;
         this.nums = num;
         this.statuss = status;
+        this.scanneds = scanned;
+        this.timestamps = timestamp;
         this.inflater = LayoutInflater.from(context);
     }
 
@@ -38,21 +43,28 @@ public class ExcelImportAdapter extends RecyclerView.Adapter<ExcelImportAdapter.
         return new ViewHolder(view);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         String title = titles.get(position);
         String desc = descriptions.get(position);
         String num = nums.get(position);
         String status = statuss.get(position);
+        String scanned = scanneds.get(position);
+        String timestamp = timestamps.get(position);
+
+
         Picasso.get().load(images.get(position)).placeholder(R.drawable.ic_launcher_background).error(R.drawable.ic_launcher_background)
                 //.resize(75,75)
                 .fit()
                 .centerCrop()
                 .into(holder.thumbnail);
         holder.title.setText(title);
-        holder.desc.setText(desc);
+        holder.desc.setText("Ref NO: "+desc);
         holder.num.setText(num);
-        holder.status.setText(status);
+        holder.scanned.setText("times: "+scanned);
+        holder.timestamp.setText("Last scan: "+timestamp);
+        holder.status.setText("In stock: " + status);
 
     }
 
@@ -62,7 +74,7 @@ public class ExcelImportAdapter extends RecyclerView.Adapter<ExcelImportAdapter.
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView title,desc,num,status;
+        TextView title,desc,num,status,scanned,timestamp;
         ImageView thumbnail;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -70,6 +82,8 @@ public class ExcelImportAdapter extends RecyclerView.Adapter<ExcelImportAdapter.
             desc = itemView.findViewById(R.id.desc);
             num = itemView.findViewById(R.id.num);
             status = itemView.findViewById(R.id.status);
+            scanned = itemView.findViewById(R.id.scantime);
+            timestamp = itemView.findViewById(R.id.lastscan);
             thumbnail = itemView.findViewById(R.id.cardImg);
 
         }
